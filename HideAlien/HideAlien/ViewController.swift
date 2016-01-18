b44ecc4235968eefe5c7a63d0a6d47ac2d5ec55c
Sarah Johnson <sarahjohnson@Sarahs-iMac.local>
2016-01-18 19:52:07 +0000

    added files for Hide the Alien

--- HideAlien/HideAlien/ViewController.swift
+++ HideAlien/HideAlien/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redAlienPic: UIImageView!
+    @IBOutlet weak var blueAlienPic: UIImageView!
+   
+    @IBOutlet weak var hideRedButton: UIButton!
+    @IBOutlet weak var hideBlueButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideTheRedAlien(sender: AnyObject) {
+        redAlienPic.hidden = true
+      
+    }
 
+    @IBAction func hideTheBlueAlien(sender: AnyObject) {
+        blueAlienPic.hidden = true 
+    }
 }
 
